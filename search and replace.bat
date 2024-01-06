37bc5cd9b8fd61bdcf463ade16fbca9a98d30b55
GravityEcho <41200150+GravityEcho@users.noreply.github.com>
2024-01-06 15:01:22 -0600

    Add files via upload

--- search and replace.bat
+++ search and replace.bat
@@ -2,16 +2,19 @@
 setlocal enabledelayedexpansion
 
 :begin
+rem Prompt user for file extensions to include in search
+set /p "search_ext=Enter file extension: "
+
 rem Prompt user for text to search for
 set /p "search_text=Enter text to search for: "
 
 rem Prompt user for replacement text
 set /p "replace_text=Enter replacement text: "
 
-rem Loop through all .m3u8 files in the directory
-for /R %%f in (*.m3u8) do (
+rem Loop through all search_ext files in the directory
+for /R %%f in (*.%search_ext%) do (
     rem Perform search and replace operation on each file
-    set "output_file=%%~nf.SR.m3u8"
+    set "output_file=%%~nf.SR.%search_ext%"
     (
         for /f "usebackq delims=" %%l in ("%%f") do (
             set "line=%%l"
